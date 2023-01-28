d0468a201a81ad3267a4ea0b93d3247104697cc3
ChristianChiarulli <chris.machine@pm.me>
2023-01-28 23:27:26 +0530

    nvim: initial lunar vim import
    kanged completely off https://github.com/LunarVim/Neovim-from-scratch/tree/master. all thanks to these people.
    Minor changes have been done to better suit me
    
    Co-authored-by: gnmearacaun <briainodriscoll@gmail.com>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

---
+++ nvim/lua/npv12/lsp/init.lua
@@ -0,0 +1,8 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
+require "npv12.lsp.mason"
+require("npv12.lsp.handlers").setup()
+require "npv12.lsp.null-ls"
