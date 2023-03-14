2dc84840382189c3bafec75321342fceb4003239
Pranav <npv12@iitbbs.ac.in>
2023-03-14 21:42:10 +0530

    kde: improve script
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- scripts/kde.zsh
+++ scripts/kde.zsh
@@ -1,7 +1,12 @@
 #! /usr/bin/zsh
 
-paru -S lightly-qt latte-dock-git kwalletmanager ksshaskpass plasma-wayland-session plasma-workspace-agent-ssh libdbusmenu-glib
+paru -S lightly-qt lightlyshaders-git latte-dock-git kwalletmanager ksshaskpass plasma-wayland-session plasma-workspace-agent-ssh libdbusmenu-glib
 git clone https://github.com/npv12/kde catppuccin-kde && cd catppuccin-kde && ./install.sh
+
 
 # Required by ssh agent to work well
 ln -s ~/dotfiles/autostart/ssh-add.desktop ~/.config/autostart/ssh-add.desktop
+
+# Assign meta to overview
+kwriteconfig5 --file kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.kglobalaccel,/component/kwin,,invokeShortcut,Overview"
+qdbus org.kde.KWin /KWin reconfigure
