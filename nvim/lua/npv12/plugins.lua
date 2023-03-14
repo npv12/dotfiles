38ffbad3b846d79afdaa3d69e89cf11bf9450002
Pranav <npv12@iitbbs.ac.in>
2023-03-14 11:39:02 +0530

    nvim: add option to make nvim transparent

--- nvim/lua/npv12/plugins.lua
+++ nvim/lua/npv12/plugins.lua
@@ -128,6 +128,9 @@ return packer.startup(function(use)
 		end,
 	})
 
+	-- Transparent bar
+	use "xiyaowong/nvim-transparent"
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
