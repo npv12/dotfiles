8caca7ba4888c1fe4509eb4ab45bdda11863ea6e
Pranav <npv12@iitbbs.ac.in>
2023-03-14 22:08:30 +0530

    latte: add my layout
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- scripts/kde.zsh
+++ scripts/kde.zsh
@@ -6,6 +6,7 @@ git clone https://github.com/npv12/kde catppuccin-kde && cd catppuccin-kde && ./
 
 # Required by ssh agent to work well
 ln -s ~/dotfiles/autostart/ssh-add.desktop ~/.config/autostart/ssh-add.desktop
+ln -s ~/dotfiles/latte/npv12.layout.latte latte/
 
 # Assign meta to overview
 kwriteconfig5 --file kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.kglobalaccel,/component/kwin,,invokeShortcut,Overview"
