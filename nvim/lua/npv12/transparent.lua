f595406b00bf8e09602448a74ebc38770bdf8ef0
Pranav <npv12@iitbbs.ac.in>
2023-05-12 09:42:25 +0530

    update transparent module
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- nvim/lua/npv12/transparent.lua
+++ nvim/lua/npv12/transparent.lua
@@ -4,12 +4,10 @@ if not status_ok then
 end
 
 configs.setup({
-    enable = true, -- boolean: enable transparent
     extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to 'all', that means all available groups
     -- example of akinsho/nvim-bufferline.lua
     "BufferLineTabClose", "BufferlineBufferSelected", "BufferLineFill", "BufferLineBackground", "BufferLineSeparator",
     "BufferLineIndicatorSelected"},
-    exclude = {}, -- table: groups you don't want to clear
-    ignore_linked_group = true -- boolean: don't clear a group that links to another group
+    exclude_groups = {}, -- table: groups you don't want to clear
 })
