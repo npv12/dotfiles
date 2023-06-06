53c9bf9e860c07a54cb852d66e926f3451d8e877
Pranav <npv12@iitbbs.ac.in>
2023-06-06 09:42:00 +0530

    update hyprland scripts
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- scripts/hyprland.zsh
+++ scripts/hyprland.zsh
@@ -1,13 +1,8 @@
-paru -S hyprland-nvidia rofi-lbonn-wayland hyprpaper-git wl-clipboard wl-clipboard-history-git xdg-desktop-portal-hyprland-git avizo
-paru -S swayidle swaylock-effects grim slurp wf-recorder eww-wayland
+paru -S ehyprland-nvidia gtklock rofi-lbonn-wayland playerctl grim slurp wf-recorder brightnessctl \
+    pavucontrol wl-gammactl wl-clipboard socat jq waybar blueberry kitty polkit-gnome swayosd \
+    wl-clipboard wl-clipboard-history-git xdg-desktop-portal-hyprland-git dunst
 
 ln -s ~/dotfiles/hypr ~/.config/hypr
 ln -s ~/dotfiles/rofi ~/.config/rofi
-
-paru -S swayidle swaylock-effects grim slurp wf-recorder foot dunst hyprland-nvidia eww-wayland ttf-segoe-ui-variable waybar \
-    bc blueberry dunst gojq light networkmanagerapplet plasma-browser-integration playerctl socat udev \
-    wlogout wofi libqalculate sox nlohmann-json boost-libs \
-    rofi-lbonn-wayland hyprpaper-git wl-clipboard wl-clipboard-history-git xdg-desktop-portal-hyprland-git bison
-
-paru -S eww-wayland hyprland-nvidia gtklock hyprpicker rofi-lbonn-wayland playerctl grim slurp wf-recorder \
-    brightnessctl pavucontrol wl-gammactl wl-clipboard socat jq waybar blueberry kitty polkit-gnome
+ln -s ~/dotfiles/dunst ~/.config/dunst
+ln -s ~/dotfiles/waybar ~/.config/waybar
