905b1c30e516c840a938beea1d94ec6d6d6d8063
Pranav <npv12@iitbbs.ac.in>
2024-08-11 22:02:13 +0530

    chore: update scripts for debian
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- scripts/setup.zsh
+++ scripts/setup.zsh
@@ -3,12 +3,30 @@
 export DIR=~/dotfiles/
 
 # Install all deps
-paru -S fd ripgrep duf eza micro bat bottom zoxide ueberzug neovim pyright
+# If paru exists
+if command -v paru &> /dev/null; then 
+    paru -S fd ripgrep duf eza micro bat bottom zoxide ueberzug neovim pyright
 
-# Fonts
-paru -S adobe-source-code-pro-fonts noto-fonts terminus-font
+    # Fonts
+    paru -S adobe-source-code-pro-fonts noto-fonts terminus-font
+fi
+
+# Debian
+if command -v apt &> /dev/null; then
+    sudo apt install -y gpg wget curl
+
+    # Eza
+    sudo mkdir -p /etc/apt/keyrings
+    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+    sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+    sudo apt update
+    sudo apt install -y eza
+
+    sudo apt install fd-find ripgrep duf micro bat zoxide ueberzug neovim
+fi 
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # Rust
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # Rust
 
 echo "Removing previous symlinks"
 rm -rf ~/.gitconfig ~/.icons ~/.config/bat ~/.config/bottom ~/.config/gdu ~/.config/micro \
@@ -29,10 +47,11 @@ ln -s ${DIR}/blackbox ~/.local/share/blackbox
 ln -s ${DIR}/mimeapps.list ~/.config/mimeapps.list
 ln -s ${DIR}/fonts ~/.local/share/fonts
 
-echo "Linking bins"
-ln -s /Coding/bins/cargo ~/.cargo
-ln -s /Coding/bins/virtualenvs ~/.virtualenvs
-ln -s /Coding/bins/nvm .nvm
-ln -s /Coding/bins/yarn .yarn/bin
+# Not required for wsl
+# echo "Linking bins"
+# ln -s /Coding/bins/cargo ~/.cargo
+# ln -s /Coding/bins/virtualenvs ~/.virtualenvs
+# ln -s /Coding/bins/nvm .nvm
+# ln -s /Coding/bins/yarn .yarn/bin
 
 bash ${DIR}/scripts/select_distro.sh
