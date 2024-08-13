7427494c50456954f246f249d0489f9c5b9a4592
Pranav <npv12@iitbbs.ac.in>
2024-08-13 14:48:32 +0530

    feat: add script to get pyenv dependencies
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

---
+++ scripts/pyenv.zsh
@@ -0,0 +1,3 @@
+echo "Installing pyenv dependencies"
+sudo apt-get update
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
