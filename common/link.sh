d3275d7220a42c8649aaa7ca0f8447e1e6d4e51f
Pranav <pranav10121@gmail.com>
2025-06-28 19:06:08 +0530

    feat: add script to link common apps
    Signed-off-by: Pranav <pranav10121@gmail.com>

---
+++ common/link.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+COMMON_DIR="$HOME/dotfiles/common"
+CONFIG_DIR="$HOME/.config"
+
+# Link ZSH
+ln -s $COMMON_DIR/zsh/zshrc ~/.zshrc
+ln -s $COMMON_DIR/zsh/zshenv ~/.zshenv
+ln -s $COMMON_DIR/zsh/zprofile ~/.zprofile
+ln -s $COMMON_DIR/zsh/config $CONFIG_DIR/zsh
+
+# Link Git
+ln -s $COMMON_DIR/git/gitconfig ~/.gitconfig
+ln -s $COMMON_DIR/git/gitignore ~/.gitignore
+
+# Link Apps
+ln -s $COMMON_DIR/alacritty $CONFIG_DIR/alacritty
+ln -s $COMMON_DIR/bat $CONFIG_DIR/bat
+ln -s $COMMON_DIR/bottom $CONFIG_DIR/bottom
+ln -s $COMMON_DIR/micro $CONFIG_DIR/micro
+ln -s $COMMON_DIR/tmux ~/.tmux.conf
