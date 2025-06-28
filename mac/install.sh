e35c27ca9e2ca6e0e340fa4082192ec92edf0f13
Pranav <pranav10121@gmail.com>
2025-06-28 19:11:55 +0530

    feat: add an install script for mac
    Signed-off-by: Pranav <pranav10121@gmail.com>

---
+++ mac/install.sh
@@ -0,0 +1,5 @@
+brew install --cask nikitabobko/tap/aerospace
+curl -sSL https://raw.githubusercontent.com/acsandmann/aerospace-swipe/main/install.sh | bash
+
+brew tap FelixKratz/formulae
+brew install sketchybar
