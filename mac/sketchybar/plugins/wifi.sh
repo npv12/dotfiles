b76f72bb6bb1d8c8c9c320f59b3e7a65737c9731
Pranav <pranav10121@gmail.com>
2026-01-28 14:50:05 +0530

    feat: wifi: truncate SSID to first MAX_LENGTH characters
    Signed-off-by: Pranav <pranav10121@gmail.com>

--- mac/sketchybar/plugins/wifi.sh
+++ mac/sketchybar/plugins/wifi.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 SSID=$(~/Applications/wifi-unredactor.app/Contents/MacOS/wifi-unredactor | jq -r '.ssid')
+
+# Truncate SSID if longer than 20 characters
+MAX_LENGTH=10
+if [ ${#SSID} -gt $MAX_LENGTH ]; then
+	SSID="${SSID:0:MAX_LENGTH}..."
+fi
 ICON=""
 
 sketchybar --set "$NAME" \
