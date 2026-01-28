b244b3d356ebb8221303bb26435d45f3c2c5a3d9
Pranav <pranav10121@gmail.com>
2026-01-28 15:08:29 +0530

    fix: wifi: correct SSID retrieval handling and ensure proper label assignment
    Signed-off-by: Pranav <pranav10121@gmail.com>

--- mac/sketchybar/plugins/wifi.sh
+++ mac/sketchybar/plugins/wifi.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
 SSID=$(~/Applications/wifi-unredactor.app/Contents/MacOS/wifi-unredactor | jq -r '.ssid')
-
-# Truncate SSID if longer than 20 characters
-MAX_LENGTH=10
-if [ ${#SSID} -gt $MAX_LENGTH ]; then
-	SSID="${SSID:0:MAX_LENGTH}..."
-fi
 ICON=""
+
+# Check if WiFi retrieval failed
+if [ "$SSID" = "failed to retrieve SSID" ]; then
+	LABEL="No Wifi"
+	ICON_COLOR="0xFFFFFFFF"
+else
+	# Truncate SSID if longer than 10 characters
+	MAX_LENGTH=10
+	if [ ${#SSID} -gt $MAX_LENGTH ]; then
+		SSID="${SSID:0:MAX_LENGTH}..."
+	fi
+	LABEL="$SSID"
+	ICON_COLOR="0xFFF38BA8"
+fi
 
 sketchybar --set "$NAME" \
 	icon="$ICON" \
-	icon.color="0xFFF38BA8" \
-	label="$SSID"
+	icon.color="$ICON_COLOR" \
+	label="$LABEL"
